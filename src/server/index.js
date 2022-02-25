3d70bc8ad89038332cbc4ad53b6990a3985f57de
Hamid Ryahi <43662608+HamidRyahi@users.noreply.github.com>
2022-02-25 17:08:37 +0100

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,79 +1,61 @@
+// import fetch from "node-fetch";
+const fetch = require('node-fetch');
 // Require hidden api key
 const dotenv = require("dotenv");
 dotenv.config();
-
+var path = require('path');
+const mockAPIResponse = require('./mockAPI.js');
+// Express to run server and routes
+const express = require('express');
 //Meaningcloud credentials for API
 const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key=";
 const API_KEY = process.env.API_KEY;
-
-var path = require('path')
-const mockAPIResponse = require('./mockAPI.js')
-const fetch = require("node-fetch");
-
-// Express to run server and routes
-const express = require('express')
-
 // Start up an instance of app
-const app = express()
-
+const app = express();
 //Middleware
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 //Origin allowance
 const cors = require("cors");
 app.use(cors());
-
-
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 // app.use(express.static('src/client/views/index.html'))
 
-
-
-console.log(__dirname)
-
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html');
 })
-
-
 const port = 8081;
-
+// designates what port the app will listen to for incoming requests
 app.listen(port, function () {
-    console.log('app listening on port 8081!')
+    console.log(`app listening on port ${port}!`);
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
-
-
-// Post Route
-app.post("/article", async (req, res) => {
-  const articleUrl = req.body.articleUrl;
-  console.log("article URL:", articleUrl);
-  const parameters = "&lang=en&url=";
-  let url = `${baseUrl}${API_KEY}${parameters}${articleUrl}`;
-  console.log(url);
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/JSON",
-    }
-  });
-  try {
-    const data = await response.json();
-    res.send({
-      score_tag: data.score_tag,
-      agreement: data.agreement,
-      subjectivity: data.subjectivity,
-      confidence: data.confidence,
-      irony: data.irony
+let projectData = {};
+app.post('/article', async (req, res) => {
+    const articleUrl = req.body.articleUrl;
+    let url = `${baseUrl}${API_KEY}${articleUrl}`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/JSON",
+        }
     });
-  } catch (err) {
-    console.log("error", err);
-  }
+    try {
+        const data = await response.json();
+        projectData['score_tag'] = data.score_tag;
+        projectData['agreement'] = data.agreement;
+        projectData['subjectivity'] = data.subjectivity;
+        projectData['confidence'] = data.confidence;
+        projectData['irony'] = data.irony;
+        console.log("server", projectData);
+        res.send(projectData);
+    } catch (error) {
+        console.log("error", error);
+    }
 });
+
+// GET route that returns the projectData object
+// app.get('/allData', (req, res) => {
+//     res.send(projectData);
+// });
