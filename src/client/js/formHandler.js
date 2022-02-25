8d6217aaaf97a1a3d5d8a5976b413849f3854387
Hamid Ryahi <43662608+HamidRyahi@users.noreply.github.com>
2022-02-25 17:19:05 +0100

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,72 +1,24 @@
-const feedback = document.querySelector('.feedback');
-
-function handleSubmit(event) {
-    event.preventDefault()
-    const formText = document.getElementById('link').value;
+export function handleSubmit(event) {
+    event.preventDefault();
+    let formText = document.getElementById('name').value;
+    const postMC = async (url = '', data = {}) => {
+        const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        try {
+            const data = await response.json();
+            Client.updateUI(data);
+            return data;
+        } catch (error) {
+            console.log("error", error);
+        }
+    }
     if (Client.validateUrl(formText)) {
-        feedback.textContent = "Successful link is submitted, please wait for the results!";
-        postMC('http://localhost:8081/article', { articleUrl: formText })
+        postMC('/article', { articleUrl: formText });
     }
 }
-
-const postMC = async (url = '', data = {}) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
-
-    try {
-        const data = await response.json();
-        console.log(data);
-        updateUI(data)
-        return data;
-    } catch (error) {
-        console.log("error", error);
-        // appropriately handle the error
-    }
-}
-
-const updateUI = (dt) => {
-    const formText = document.getElementById('link').value;
-    const kjkj = document.getElementById('sub_link');
-    kjkj.style.display = 'block';
-    kjkj.setAttribute('href', `${formText}`);
-    kjkj.textContent = formText;
-    feedback.textContent = "";
-    document.getElementById('results').style.display = 'flex';
-    document.getElementById('polarity').innerText = polarity(dt.score_tag)
-    document.getElementById('agreement').innerText = dt.agreement
-    document.getElementById('subjectivity').innerText = dt.subjectivity
-    document.getElementById('confidence').innerText = dt.confidence + "%"
-    document.getElementById('irony').innerText = dt.irony
-}
-
-function polarity(scoreTag) {
-    switch (scoreTag) {
-        case "P":
-            scoreTag = "POSITIVE";
-            break;
-        case "P+":
-            scoreTag = "STRONG POSITIVE";
-            break;
-        case "NEU":
-            scoreTag = "NEUTRAL";
-            break;
-        case "N":
-            scoreTag = "NEGATIVE";
-            break;
-        case "N+":
-            scoreTag = "STRONG NEGATIVE";
-            break;
-        case "NONE":
-            scoreTag = "WITHOUT SENTIMENT";
-            break;
-    }
-    return scoreTag;
-}
-
-export { handleSubmit, polarity }
