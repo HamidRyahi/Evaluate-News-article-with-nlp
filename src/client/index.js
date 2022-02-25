ac2035ed7d5af73eff5d504ddd8f05d00ea384ed
Hamid Ryahi <43662608+HamidRyahi@users.noreply.github.com>
2022-02-25 17:09:20 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,18 @@
-import { validateUrl } from './js/urlChecker'
-import { handleSubmit } from './js/formHandler'
+import { handleSubmit } from './js/formHandler';
+import { updateUI } from './js/updateUI';
+import { validateUrl } from './js/urlChecker';
+import { polarity } from './js/polarity';
+
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+import './styles/main.scss'
 
 export {
+    handleSubmit,
+    updateUI,
     validateUrl,
-    handleSubmit
+    polarity
 }
